7497775023d4a5365b8581b000d2fee7a8031341
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-05-21 15:16:20 -0400

    updated form structure

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,6 +12,27 @@ const Form = props => {
                 name='name'
                 placeholder='Member Name'
             />
+            <label htmlFor='email'>Email</label>
+            <input
+                id='email'
+                type='email'
+                name='email'
+                placeholder='email'
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+                id='password'
+                type='password'
+                name='password'
+                placeholder='password'
+            />
+            <label htmlFor='terms' className='terms'>Terms and Conditions</label>
+            <input
+                type='checkbox'
+                name='terms'
+                checked={true} //Change this later to be dynamic
+            />
+            <button type='submit'>Submit Form</button>
         </form>
 
     )

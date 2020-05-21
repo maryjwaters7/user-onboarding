5807ebcd267edc664d8113ab7139596e7ae2ead6
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-05-21 14:01:27 -0400

    updated basic form structure

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,2 +1,20 @@
 import React from 'react';
 
+const Form = props => {
+
+    return (
+
+        <form>
+            <label htmlFor='name'>Name</label>
+            <input 
+                id='name'
+                type='text'
+                name='name'
+                placeholder='Member Name'
+            />
+        </form>
+
+    )
+};
+
+export default Form;
